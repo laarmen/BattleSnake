9d09ce084fe40b348d26f9183598a17af92d020a
Simon Chopin <chopin.simon@gmail.com>
2021-07-10 19:42:01 +0000

    S'more boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
